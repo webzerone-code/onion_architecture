47a92bb01444fd675086661426959f04fc373484
Mohamed Reda <webzerone@gmail.com>
2025-10-18 22:18:50 +0300

    Repositories

---
+++ app/infrastructure/persistence/OrderRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\infrastructure\persistence;
+
+use App\architecture\onion\domain\entity\AbstractEntity;
+use App\architecture\onion\domain\repository\OrderRepositoryInterface;
+
+class OrderRepository implements OrderRepositoryInterface
+{
+
+    public function create(AbstractEntity $entity): int
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function readById(string $id): AbstractEntity
+    {
+        // TODO: Implement readById() method.
+    }
+
+    public function readAll(): array
+    {
+        // TODO: Implement readAll() method.
+    }
+
+    public function update(AbstractEntity $entity): AbstractEntity
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete(AbstractEntity $entity): AbstractEntity
+    {
+        // TODO: Implement delete() method.
+    }
+}
